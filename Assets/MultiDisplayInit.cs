af78b44a3369e2d7984c03b2153134a78b3daa87
sk <751933592@qq.com>
2018-07-31 09:26:55 +0800

    up

---
+++ Assets/MultiDisplayInit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiDisplayInit : MonoBehaviour
+{
+    public int displayCount;
+
+    // Use this for initialization
+    void Start()
+    {
+        int count = Mathf.Min(displayCount, Display.displays.Length);
+        int mainWidth = Display.main.systemWidth;
+        int mainHeight = Display.main.systemHeight;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (Display.displays[i].active)
+                continue;
+            Display.displays[i].Activate(mainWidth, mainHeight, 60);
+        }
+    }
+}
